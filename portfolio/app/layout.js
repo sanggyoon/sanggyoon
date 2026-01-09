68b64a03ea3358894df8ea64906cbd2c5915978e
sanggyoon <sangreal99@naver.com>
2026-01-09 18:13:30 +0900

    VariableProximity doens't works

--- portfolio/app/layout.js
+++ portfolio/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import '../styles/globals.css';
+import '../styles/VariableProximity.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,16 +1,24 @@
 'use client';
 
-import styles from './page.module.css';
+// 임포트
+import { useRef } from 'react';
+
+// 모듈 스타일
+// import styles from './page.module.css';
+
 // react bits 컴포넌트 가져오기
 import Lanyard from '@/components/Lanyard';
 import LightRays from '@/components/LightRays';
+import VariableProximity from '@/components/VariableProximity';
 
 export default function Home() {
+  const containerRef = useRef(null);
+
   return (
-    <div className={styles.page}>
+    <>
       <LightRays
         raysOrigin="top-center"
-        raysColor="#00ffff"
+        raysColor="#ffffffff"
         raysSpeed={1.5}
         lightSpread={0.8}
         rayLength={1.2}
@@ -21,6 +29,35 @@ export default function Home() {
         className="custom-rays"
       />
       <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
-    </div>
+
+      <div
+        ref={containerRef}
+        style={{
+          position: 'relative',
+          minHeight: '100vh', // 전체 화면 높이
+          padding: '50px', // 충분한 패딩
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <VariableProximity
+          label={
+            'Hover me! And then star React Bits on GitHub, or else... l;askdfjl;sadkfj;llakfsd'
+          }
+          style={{
+            fontSize: '3rem', // 큰 폰트 크기 추가
+            fontWeight: 400,
+            lineHeight: 1.2,
+          }}
+          className="variable-proximity"
+          fromFontVariationSettings="'wght' 400, 'opsz' 9"
+          toFontVariationSettings="'wght' 1000, 'opsz' 40"
+          containerRef={containerRef}
+          radius={100}
+          falloff="linear"
+        />
+      </div>
+    </>
   );
 }
