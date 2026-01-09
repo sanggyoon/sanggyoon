0a01d498ef6d7c837c672cc26398e1e075825da7
sanggyoon <sangreal99@naver.com>
2026-01-09 17:27:01 +0900

    stack lanyard on lightrays

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -8,21 +8,19 @@ import LightRays from '@/components/LightRays';
 export default function Home() {
   return (
     <div className={styles.page}>
-      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
-        <LightRays
-          raysOrigin="top-center"
-          raysColor="#00ffff"
-          raysSpeed={1.5}
-          lightSpread={0.8}
-          rayLength={1.2}
-          followMouse={true}
-          mouseInfluence={0.1}
-          noiseAmount={0.1}
-          distortion={0.05}
-          className="custom-rays"
-        />
-        <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
-      </div>
+      <LightRays
+        raysOrigin="top-center"
+        raysColor="#00ffff"
+        raysSpeed={1.5}
+        lightSpread={0.8}
+        rayLength={1.2}
+        followMouse={true}
+        mouseInfluence={0.1}
+        noiseAmount={0.1}
+        distortion={0.05}
+        className="custom-rays"
+      />
+      <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
     </div>
   );
 }
